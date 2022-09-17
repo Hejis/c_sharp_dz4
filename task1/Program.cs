26d341f67d122676ea244ead194a16e7e425b42b
simpaa_comp <simpaa@inbox.ru>
2022-09-17 13:38:27 +0300

    task2

---
+++ task1/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите цикл, который принимает 
+//на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+
+//3, 5 -> 243 (3⁵)
+
+//2, 4 -> 16
+
+int Prompt(string message)
+{
+  Console.Write(message);
+  string StringValue = Console.ReadLine() ?? "0";
+  bool isNumber = int.TryParse(StringValue, out int value);
+  if (isNumber)
+  {
+    return value;
+  }
+  throw new Exception("Данное значение невозможно преобразовать в число");
+
+}
+
+
+int Stepen(int numberA, int numberB){
+  int result = 1;
+  for(int i=1; i <= numberB; i++){
+    result = result * numberA;
+  }
+    return result;
+}
+
+  
+  int numberA =Prompt("Введите число A: ");
+
+  int numberB = Prompt("Введите число B: ");
+
+  int stepen = Stepen(numberA, numberB);
+  Console.WriteLine($"{numberA}^{numberB} =  {stepen}");
---
+++ task2/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+//452 -> 11
+
+//82 -> 10
+
+//9012 -> 12
+
+int Prompt(string message)
+{
+  Console.Write(message);
+  string StringValue = Console.ReadLine() ?? "0";
+  bool isNumber = int.TryParse(StringValue, out int value);
+  if (isNumber)
+  {
+    return value;
+  }
+  throw new Exception("Данное значение невозможно преобразовать в число");
+
+}
+
+
+
+  int Sum(int numberN){
+    
+    int counter = Convert.ToString(numberN).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++){
+      advance = numberN - numberN % 10;
+      result = result + (numberN - advance);
+      numberN = numberN / 10;
+    }
+   return result;
+  }
+int numberN = Prompt("Введите число N: ");
+int sum = Sum(numberN);
+Console.WriteLine($"Сумма цифр в числе:  {sum}");
