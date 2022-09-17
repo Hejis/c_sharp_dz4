2ca95025f44427c78083624f149b8c2ab739aa72
simpaa_comp <simpaa@inbox.ru>
2022-09-17 13:49:57 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+//6, 1, 33 -> [6, 1, 33]
+   
+
+
+   int Prompt(string message)
+{
+  Console.Write(message);
+  string StringValue = Console.ReadLine() ?? "0";
+  bool isNumber = int.TryParse(StringValue, out int value);
+  if (isNumber)
+  {
+    return value;
+  }
+  throw new Exception("Данное значение невозможно преобразовать в число");
+
+}
+
+Random rnd = new Random();
+int len = Prompt("Введите длину массива: ");
+int[] a = new int[len];
+for (int i = 0; i < a.Length; i++)
+{
+    a[i] =  rnd.Next(1,1000);
+}
+for (int i = 0; i < a.Length; i++)
+{
+    Console.WriteLine($"a[{i}] = {a[i]}");
+}
